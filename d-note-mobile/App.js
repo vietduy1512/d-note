bf9b748890f66de99012bc64008b04be578153e2
Duy <vietduy1512@gmail.com>
2020-02-25 02:27:44 +0700

    Add styling

--- d-note-mobile/App.js
+++ d-note-mobile/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 function HomeScreen() {
   return (
@@ -21,10 +22,37 @@ function SettingsScreen() {
 
 const Tab = createBottomTabNavigator();
 
+const screenOptions = ({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+
+    switch (route.name) {
+      case 'Home':
+        iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+        break;
+      case 'Settings':
+        iconName = focused ? 'ios-list-box' : 'ios-list';
+        break;
+      default:
+        break;
+    }
+
+    return <Ionicons name={iconName} size={size} color={color} />;
+  },
+})
+
+const tabBarOptions = {
+  activeTintColor: 'tomato',
+  inactiveTintColor: 'gray',
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={screenOptions}
+        tabBarOptions={tabBarOptions}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
